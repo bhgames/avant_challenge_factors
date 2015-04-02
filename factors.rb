3aad538b120a7634f60ee2d3346011b9503e13f9
William Burke <burke187@gmail.com>
2015-04-02 10:15:51 -0500

    final commit

--- factors.rb
+++ factors.rb
@@ -1,7 +1,9 @@
 def factors(numbers)
-	numbers.sort.each_with_object({}).each_with_index {|(int, result), index| 
+	numbers.sort!.each_with_object({}).each_with_index{|(int, result), index| 
 		factor = numbers[0, index]
-    	results = factor.select {|f| int % f == 0 } 
+    	results = factor.select {|f| (int % f).zero? } 
     	result[int] = results 
 	}
 end
+numbers = [10, 5, 2, 20]
+p factors(numbers)
--- spec/factors_spec.rb
+++ spec/factors_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../factors'
 
 describe 'factors' do
-	numbers = [20, 4, 5, 15, 30, 12]
+	let(:numbers){[20, 4, 5, 15, 30, 12]}
 
 	it 'should return hash table of factors' do 
 		expect(factors(numbers)).to eq({4=>[], 5=>[], 12=>[4], 15=>[5], 20=>[20, 4, 5], 30=>[5, 15, 30]})
